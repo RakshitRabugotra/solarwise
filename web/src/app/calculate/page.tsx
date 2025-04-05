c6852cefbb16cf653fa7157fa905045e3db2fe32
RakshitRabugotra <rakshit.rabugotra000@gmail.com>
2025-04-05 21:55:14 +0530

    feat: add footer to the calculation page

--- web/src/app/calculate/page.tsx
+++ web/src/app/calculate/page.tsx
@@ -1,10 +1,12 @@
 import CalculateMainContent from "@/components/page/calculate/CalculateMainContent"
+import Footer from "@/components/page/home/Footer"
 
 export default function CalculatePage() {
   return (
     <main className="min-h-screen w-full">
       {/* The main content for the calculate page */}
       <CalculateMainContent />
+      <Footer />
     </main>
   )
 }
